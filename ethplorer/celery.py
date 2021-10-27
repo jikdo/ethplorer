788e59d5fb538d9c5f130aab43cd1898c19c6db2
Jikdo <jikdo_@outlook.com>
2021-10-27 22:58:39 +0000

    added test celery task

--- ethplorer/celery.py
+++ ethplorer/celery.py
@@ -3,7 +3,7 @@ from celery import Celery
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ethplorer.settings')
-app =  Celery('ethplorer')
+app = Celery('ethplorer')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 
--- indexer/tasks.py
+++ indexer/tasks.py
@@ -1,9 +1,54 @@
+from django.db.utils import Error
 from celery import shared_task
+from celery.utils.log import get_task_logger
+from web3 import Web3
+import traceback
+
+from django.conf import settings
+from .utils import (
+    get_account_type
+)
+
 from .models import (
     Block, Transaction,
     Account
 )
 
+logger = get_task_logger(__name__)
+
+
 @shared_task
-def index_blockchain(block_height):
-    pass
+def index_blockchain(start_block_id):
+    logger.info('lets work')
+    # initialize web3
+    provider = Web3.HTTPProvider(settings.WEB3_PROVIDER)
+    w3 = Web3(provider)
+
+    # fetch blockchain data
+    start_block = start_block_id
+    current_block_number = w3.eth.block_number
+    if (start_block > current_block_number):
+        return f'Error: start_block must be <= current block height - {current_block_number}'
+
+    while(True):
+
+        try:
+
+            logger.info('something is happening')
+            block_response = w3.eth.get_block(start_block)
+
+            miner = Account.objects.create(
+                hash=block_response.miner,
+                account_type=get_account_type(block_response.miner, w3)
+            )
+
+            # block = Block.objects.create(
+
+            # )
+
+        except Exception as e:
+            traceback.print_exception(e)
+            logger.info('error happend here')
+            pass
+
+        break
